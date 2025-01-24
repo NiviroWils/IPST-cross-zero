d597867a59452262dbf553093536e9fac0147898
NiviroWils <145367856+NiviroWils@users.noreply.github.com>
2025-01-25 01:18:19 +0700

    Delete cross-zero directory

--- cross-zero/src/shared/utils/make-class-name/index.ts
+++
@@ -1,7 +0,0 @@
-type Classname = string | undefined | null | boolean
-
-const makeClassname = (...classnames: Classname[]): string => {
-	return classnames.filter((item) => typeof item === 'string').join(' ');
-};
-
-export { makeClassname };
